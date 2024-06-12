05d5205bca86d074a29b78242a5d93ab20101c26
Josue Emaxs <josueemaxsalazar04@gmail.com>
2024-06-12 11:05:44 -0500

    :memo: Big-O: Rule Book 3 - different terms for inputs

---
+++ Big_O/Rule_Book/03_different-terms-for-inputs.js
@@ -0,0 +1,11 @@
+function compressBoxesTwice(boxes, boxes2) {
+	boxes.forEach((boxes) => {
+		console.log(boxes);
+	});
+
+	boxes2.forEach((boxes) => {
+		console.log(boxes);
+	});
+}
+
+// O(a + b)
