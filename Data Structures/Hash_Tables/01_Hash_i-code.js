d2bc4f270717a8c487ddf3d7615d5f720a3ce962
Josue Emaxs <josueemaxsalazar04@gmail.com>
2024-07-01 03:09:58 -0500

    :package: DS _ HashTable - initial code

---
+++ Data Structures/Hash_Tables/01_Hash_i-code.js
@@ -0,0 +1,17 @@
+class HashTable {
+	constructor(size) {
+		this.data = new Array(size);
+	}
+
+	_hash(key) {
+		let hash = 0;
+		for (let i = 0; i < key.length; i++) {
+			hash = (hash + key.charCodeAt(i) * i) % this.data.length;
+		}
+		return hash;
+	}
+}
+
+const myHashTable = new HashTable(50);
+myHashTable.set('bananas', 1000);
+myHashTable.get('bananas');
